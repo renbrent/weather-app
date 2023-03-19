585eec41c8523376b45a7b8c62da029ce379b7df
renbrent <renbrent23@gmail.com>
2023-03-19 00:45:03 -0700

    created codebase

--- src/App.js
+++ src/App.js
@@ -1,30 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CardContent  from '@mui/material/CardContent';
+import CardHeader from '@mui/material/CardHeader';
+import Container from '@mui/material/Container'
+import CssBaseline from '@mui/material/CssBaseline';
+import Grid from '@mui/material/Grid'
+import Thunderstorm from '@mui/icons-material/Thunderstorm';
+import Typography from '@mui/material/Typography'
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme();
 
 
-function App() {
+
+export default function App() {
+  let items=['Item 1','Item 2','Item 3','Item 4','Item 5'];
+  let itemsList = items.map((item)=>{
+    return (
+      <Card>
+        <CardHeader
+          style={{textAlign:'center'}}
+          title={item}
+        />
+        <CardContent>
+          This is {item}.
+        </CardContent>
+      </Card>
+    )
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="md">
+        <CssBaseline />
+        <Card
+            sx={{
+              marginTop: 8,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              borderRadius: '16px',
+            }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <Thunderstorm/>
+          </Avatar>
+          <Typography variant="h5" color="initial">
+            Weather App
+          </Typography>
+          <Typography variant="h5" color="initial">
+            Today's Weather
+          </Typography>
+          <Grid
+            sx={{m:4}}
+            container
+            spacing={2}
+            columns={5}
+            alignItems="center"
+            justifyContent="center"
+          >
+            {itemsList}
+          </Grid>
+        </Card>
+      </Container>
+    </ThemeProvider>
   );
 }
 
-export default App;
